5a4cd9bd860b046cf1b12724f85a6376b486d70f
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-11-24 20:25:41 -0300

    Feat: CICD workflow

--- entities/transaction.ts
+++ entities/transaction.ts
@@ -12,7 +12,7 @@ class Transaction {
   }
 
   public toString() {
-    return `Account Number: ${this.accountNumber} | Amount: ${this.amount} | Date: ${this.date.toString()} | Operation: ${this.operation}`;
+    return `Account Number: ${this.accountNumber} - Amount: ${this.amount} - Date: ${this.date.toString()} - Operation: ${this.operation}`;
   }
 }
 
