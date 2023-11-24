a73337949853157d0701d0f0a7d46cd0564417a3
Gabriel Teixeira <Gteixeiraca@gmail.com>
2023-11-24 20:25:09 -0300

    Feat: Unit Tests

---
+++ entities/account.test.ts
@@ -0,0 +1,39 @@
+import BankAccount from "./account";
+
+describe("BankAccount", () => {
+  let account: BankAccount;
+
+  beforeEach(() => {
+    account = new BankAccount(1000, 1);
+  });
+
+  it("should deposit the specified amount into the account", () => {
+    account.deposit(500);
+    expect(account.getBalance()).toBe(1500);
+  });
+
+  it("should withdraw the specified amount from the account", () => {
+    account.withdraw(200);
+    expect(account.getBalance()).toBe(800);
+  });
+
+  it("should return the current balance of the account", () => {
+    expect(account.getBalance()).toBe(1000);
+  });
+
+  it("should return the account number", () => {
+    expect(account.getNumber()).toBe(1);
+  });
+
+  it("should return the statement of transactions", () => {
+    const date = new Date(2020, 1, 1);
+    const spy = jest.spyOn(global, "Date").mockImplementation(() => date);
+
+    account.deposit(500);
+    account.withdraw(200);
+
+    expect(account.getStatement()).toEqual("Account Number: 1 | Amount: 500 | Date: Sat Feb 01 2020 00:00:00 GMT-0300 (Brasilia Standard Time) | Operation: Deposit\nAccount Number: 1 | Amount: 200 | Date: Sat Feb 01 2020 00:00:00 GMT-0300 (Brasilia Standard Time) | Operation: Withdrawal\n");
+  
+    spy.mockRestore();
+  });
+});
---
+++ entities/transaction.test.ts
@@ -0,0 +1,15 @@
+import Transaction from './transaction';
+
+describe('Transaction', () => {
+  it('should return the correct string representation of the transaction', () => {
+    const date = new Date(2020, 1, 1);
+    const spy = jest.spyOn(global, 'Date').mockImplementation(() => date);
+    const transaction = new Transaction(1, 500, 'Deposit');
+  
+    expect(transaction.toString()).toBe(
+      `Account Number: 1 | Amount: 500 | Date: ${date} | Operation: Deposit`,
+    );
+    
+    spy.mockRestore();
+  });
+});
